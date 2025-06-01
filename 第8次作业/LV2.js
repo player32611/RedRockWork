b2d47ab69de73874d35296e373aa2bdfc0e00531
player32611 <hcb326630@163.com>
2025-06-01 19:01:24 +0800

    初始化

--- 第8次作业/LV2.js
+++ 第8次作业/LV2.js
@@ -3,7 +3,7 @@ const url = `https://api.github.com/user/repos`
 const res = await fetch(url, {
     method: 'POST',
     headers: {
-        'Authorization': `token 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`,
+        'Authorization': `token`,
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({
